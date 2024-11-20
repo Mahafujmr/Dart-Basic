4f471c2d15b246d127c7cda76ab016b3c858cc2a
Mahafujmr <mrtuhin444@gmail.com>
2024-11-21 00:33:28 +0530

    dart keyword list

---
+++ dart_basic/dart_keyword_list.dart
@@ -0,0 +1,43 @@
+// dart  keyword list below
+
+/*
+Dart keywords are reserved words or terms in the Dart programming
+language that the compiler interprets differently.
+
+............
+These keywords can’t be used as the name
+of a variable, class, or function.
+............
+Dart keywords are case sensitive and must be defined properly.
+
+ */
+
+
+
+
+/*
+// dart keyword
+
+................
+abstract , else , import ,super ,as ,enum. in ,switch.assert,export
+interface, sync , async ,extend .is await .extension ,library
+.............
+
+
+...............
+case ,factory ,new ,try ,class ,final ,catch ,false ,null typedef
+on ,const finally ,operator ,void continue ,for
+...............
+
+
+
+..............
+part,while ,covariant ,function ,rethrow ,with ,default
+get ,return ,deferred ,hide set
+..............
+
+...........
+do ,if, show ,dynamic ,implements ,static
+............
+
+ */
