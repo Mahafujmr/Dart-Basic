44c2d08b9db960ad8683ee41c9d34f50339f1552
Mahafujmr <mrtuhin444@gmail.com>
2024-11-22 21:08:06 +0530

    dart data type

---
+++ dart_data_type/data_type_syntax.dart
@@ -0,0 +1,109 @@
+// dart data type details
+
+/*
+// dart all data types
+
+
+...........
+1. Numbers
+Used for numerical values.
+
+int: Whole numbers (e.g., 1, 100, -45).
+double: Decimal numbers (e.g., 3.14, -0.5)
+
+ Numbers
+int: Represents integer values (whole numbers).
+Example: int age = 25;
+double: Represents floating-point numbers (decimal values).
+Example: double pi = 3.14;
+num: A superclass of int and double. It can hold either type.
+Example: num value = 42;
+
+//
+int age = 25;
+double pi = 3.14;
+................//
+
+
+
+
+..............
+2. Strings
+Used for text representation.
+Example: String name = "Dart Programming";
+Strings can be defined with either single (') or double (") quotes.
+Example: 'Hello' or "World"
+
+String description = '''This
+is
+Dart.''';
+String name = "John";
+.................///
+
+
+
+
+...........
+3. Booleans
+bool: Represents true or false.
+Example: bool isFlutterFun = true;
+Used for true or false values.
+bool: Logical values (true or false).
+
+
+bool isLoggedIn = true;
+................
+
+
+ ...............
+4. Lists
+Used for ordered collections of items.
+
+List: A collection of values (like an array).
+List<int> numbers = [1, 2, 3];
+..............
+
+ ............
+5. Maps
+Used for key-value pairs.
+Map: A collection of key-value pairs.
+Map<String, int> phoneBook = {"Alice": 123, "Bob": 456};
+..................
+
+
+ ............
+6. Sets
+Represents an unordered collection of unique items
+Set<String> colors = {'red', 'green', 'blue'};
+.............
+
+...............
+7. Dynamic
+Used when you don’t know the type in advance.
+dynamic: Can hold any type of value.
+dynamic anything = 42;
+anything = "Hello"; // Works fine
+dynamic value = 42;
+value = "Now I'm a string!";
+....................
+
+8. Null Safety
+Dart uses the null type to represent a value that is absent.
+ With null safety enabled:
+A variable can't contain null unless explicitly allowed using ?.
+int? nullableInt;
+nullableInt = null; // Allowed
+...........
+Think of variables as boxes:
+
+An int box can only hold numbers like 1, 2, or 3.
+A String box can only hold text like "Hello".
+A bool box can hold true or false answers.
+A dynamic box is special—it can hold anything.
+
+ */
+
+
+
+
+
