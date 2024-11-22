1977dd560a3e588ec290a38c63dfbfdeee98b822
Mahafujmr <mrtuhin444@gmail.com>
2024-11-22 21:49:10 +0530

    variable

---
+++ dart_variable/variable.dart
@@ -0,0 +1,22 @@
+// dart variable
+void main(){
+  var a = 20; // variable declare = var , variable a , value 20
+  var b = 10; // variable declare = var , variable b , value 10
+  print(a+b);
+
+  var name ='tuhin';
+  var age = 20;
+  print('this is my information ${name} ${age}');
+
+  var x = 300;
+  var y = 200;
+  print(x+y);
+
+
+  var  address='kustia';
+  var taka=300;
+  print('my name is ${address}');
+  print('my taka is $taka');
+
+
+}
