83c5b241cf624501b4659791990a81557ee2164a
Mahafujmr <mrtuhin444@gmail.com>
2024-11-22 11:49:10 +0530

    dart variable

---
+++ dart_basic/dart_variable/variable_syntax.dart
@@ -0,0 +1,25 @@
+// dart variable all details
+
+/*
+Rules and Conventions for Declaring Variables in Dart *
+ -Here are the guidelines you should -
+follow when declaring variables in Dart:
+
+..............
+1) Rules for Naming Variables
+Start with a Letter or _ (Underscore):
+
+Variable names must start with a letter (a-z or A-Z)
+or an underscore (_).
+Names cannot start with a digit (0-9).
+
+var name = 'John';  // Valid
+var _age = 25;      // Valid
+var $salary = 5000; // Valid
+.......................
+
+
+
+
+
+ */
