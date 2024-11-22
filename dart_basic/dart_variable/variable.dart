1ca8e751a7a97aca437479b64da458c87442af57
Mahafujmr <mrtuhin444@gmail.com>
2024-11-22 19:04:13 +0530

    dart variable

--- dart_basic/dart_variable/variable.dart
+++ dart_basic/dart_variable/variable.dart
@@ -8,4 +8,15 @@ void main(){
   var age = 20;
   print('this is my information ${name} ${age}');
 
+  var x = 300;
+  var y = 200;
+print(x+y);
+
+
+  var  address='kustia';
+  var taka=300;
+  print('my name is ${address}');
+  print('my taka is $taka');
+
+
 }
