e0dd7e3a24fd1e5eaf29ba717f3a68d077bf7d20
Mahafujmr <mrtuhin444@gmail.com>
2024-11-22 21:47:14 +0530

    dart number

---
+++ dart_basic/number.dart
@@ -0,0 +1,22 @@
+// dart number
+// dart number = int or double
+// int = 2,3,44,100,-9
+// double = 11.23, 33.4, 55.66
+void main(){
+  var a = 22;
+  var b =10;
+  print(a+b);
+
+  var c = 20;
+  var d = 10;
+  var e= c-d;
+  print(a-b);
+  print(c-d);
+  print(e);
+
+  // dart data type in number
+  // use int or double
+  int number = 30;
+  int number1 = 50;
+  print(number1+number);
+}
