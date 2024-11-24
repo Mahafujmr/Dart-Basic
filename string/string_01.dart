be19e2b33c29288be78021d832a43fe9958f32aa
Mahafujmr <mrtuhin444@gmail.com>
2024-11-24 22:30:43 +0530

    dart string

---
+++ string/string_01.dart
@@ -0,0 +1,11 @@
+// dart string
+// string means = name, address , English word , symbol
+// string store single quotes ('')
+// string store double quotes ( "")
+
+void main(){
+  var name = "tuhin";
+  var name1 = 'Mahafuj';
+  print('My name is $name');
+  print('My other name is $name1');
+}
