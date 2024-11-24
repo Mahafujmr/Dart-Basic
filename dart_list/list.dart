e9d2b389d9b68d4e31ade2d6bb4ae19a158e7431
Mahafujmr <mrtuhin444@gmail.com>
2024-11-24 23:23:11 +0530

    list of dart

--- dart_list/list.dart
+++ dart_list/list.dart
@@ -3,4 +3,7 @@
 void main(){
   var myCity = ['Dhaka','khulna','Rangpur'];
   print('My city List $myCity');
+
+List<int> studentRollNo=  [1,2,3,4,5,6];
+print('My student Roll Number List $studentRollNo');
 }
