da322d6d5e0644098bc7a3b0ec623fd0bb315ad0
Mahafujmr <mrtuhin444@gmail.com>
2024-11-24 22:42:41 +0530

    dart String

---
+++ string/string_02.dart
@@ -0,0 +1,14 @@
+// dart string
+// problem my information
+
+void main(){
+  String name ='tuhin';
+  String office_name='Tech Hoop Bd';
+  String house_name ='Kushtia';
+  String institute_name ='SPI';
+
+  print('My name is = ${name}');
+  print('My Office name is = ${office_name}');
+  print('My House name is = ${house_name}');
+  print('My Institute name is = ${institute_name}');
+}
