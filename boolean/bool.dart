f875d1a11ae71161adedd12dcd74cfc86fa1f542
Mahafujmr <mrtuhin444@gmail.com>
2024-11-24 22:56:18 +0530

    dart boolean

---
+++ boolean/bool.dart
@@ -0,0 +1,10 @@
+// dart boolean = bool
+// dart bool use to = true or false
+void main(){
+  var isBangladesh = true;
+  var isNotBangladesh = false;
+
+
+  print('My country name is Bangladesh $isBangladesh');
+  print('My country is not India $isNotBangladesh ');
+}
