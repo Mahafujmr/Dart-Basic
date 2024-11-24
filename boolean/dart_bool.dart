4af84bae2d00602310346ece6df6429d9934d013
Mahafujmr <mrtuhin444@gmail.com>
2024-11-24 23:06:39 +0530

    bool dart

---
+++ boolean/dart_bool.dart
@@ -0,0 +1,8 @@
+// dart bool
+void main(){
+  bool myName = true; // bool type
+  bool my_Home_Name =false; // bool type
+
+  print('My name is Tuhin This is ${myName}');
+  print('My Home Town is Dhaka This is ${my_Home_Name}');
+}
