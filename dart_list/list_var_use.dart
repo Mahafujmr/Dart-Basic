773e6c42a04e3c703254c830eee18ca7a8261d8f
Mahafujmr <mrtuhin444@gmail.com>
2024-11-26 00:13:42 +0530

    dart list type

---
+++ dart_list/list_var_use.dart
@@ -0,0 +1,13 @@
+// dart list
+// dart list use var variable
+// dart list store any type data or value
+void main(){
+  var list = ['Dhaka','Tuhin',1,10,200.33, true ,false];
+  // dart var type variable store any type value
+  // int, double, String, boolean
+  print(list);
+  print('This is int Type =  ${list[3]}');
+  print('This is double Type = ${list[4]}');
+  print("This is String Type = ${list[2]}");
+  print('This is Boolean Type = ${list[5]}');
+}
