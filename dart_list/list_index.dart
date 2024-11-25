21e7aecebbadcfd77ba970c533f8b67e2dbc7646
Mahafujmr <mrtuhin444@gmail.com>
2024-11-26 00:06:22 +0530

    dart list use index number

---
+++ dart_list/list_index.dart
@@ -0,0 +1,15 @@
+// dart list index
+/// dart list syntax = List = [];
+/// list index number number start = 0
+/// list index number =0,1,2,3,4,5
+
+void main(){
+  List<String> name = ['Dhaka','Khulna','Rangpur','Nator'];
+  print(name);
+  // only one element access this list
+  // one element access this list use index number
+  // index number call []
+  print(name[0]);  // index number 0 // output = dhaka
+  print(name[3]);
+
+}
