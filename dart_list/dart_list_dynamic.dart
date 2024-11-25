44e96b1a3e295a02ff9cd7871023b4008f612616
Mahafujmr <mrtuhin444@gmail.com>
2024-11-26 00:18:05 +0530

    dart dynamic type list

---
+++ dart_list/dart_list_dynamic.dart
@@ -0,0 +1,18 @@
+// dart list
+// dart list dynamic type
+void main(){
+  dynamic info =[100,22.33, true ,false, 'Tuhin','Toma','Kushtia'];
+  print(info);
+
+  // index number start =0 ,1,2,3,4,5,6
+  print(info[1]);
+  print(info[2]);
+  print(info[3]);
+  print(info[4]);
+  print(info[5]);
+  print('This is Dynamic Type List = ${info}');
+
+}
+
+// dynamic type store any type data or value
+//like dynamic type keyword store = int, double, String, bool
