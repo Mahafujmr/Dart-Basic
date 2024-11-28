0901b9c0b58b74469fb0694cfaae7119c9ac2378
Mahafujmr <mrtuhin444@gmail.com>
2024-11-29 00:17:18 +0530

    map in dart

---
+++ dart_map/map/dart_map.dart
@@ -0,0 +1,11 @@
+// dart map
+void main(){
+  dynamic person = {
+    'name':'MR Tuhin',
+    'age':22,
+    'address':'Kushita , Daulatpur ',
+    'Study':'Shariatpur polytechnic institute',
+    'Department ': 'Computer',
+  };
+  print(person);
+}
