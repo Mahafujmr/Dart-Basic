7cf527cfcac1040ba3d804a1d82e2043530ba4e4
Mahafujmr <mrtuhin444@gmail.com>
2024-11-29 00:04:42 +0530

    dart map

---
+++ dart_map/dart_map.dart
@@ -0,0 +1,13 @@
+/// dart map
+///   ||
+///   Map all element store {};
+///   dart map = key value = key : value
+
+void main(){
+  var person = {
+    'name ': 'Jhon',
+    'age': 22,
+    'address': 'London'
+  };
+  print(person);
+}
